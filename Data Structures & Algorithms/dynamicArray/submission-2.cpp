cb661847a2382ff7d4f8aaaf8c2e177a5751773c
jj <168054202+mightyjj@users.noreply.github.com>
2026-09-08 10:58:00 +1000

    Add: dynamicArray - submission-2

---
+++ Data Structures & Algorithms/dynamicArray/submission-2.cpp
@@ -0,0 +1,49 @@
+class DynamicArray {
+    int* arr;
+    int size;
+    int capacity;
+
+public:
+
+    DynamicArray(int capacity) 
+        : arr(new int[capacity])
+        , size(0)
+        , capacity(capacity) {}
+
+    ~DynamicArray() { delete[] arr; }
+
+    int get(int i) {
+        return arr[i];
+    }
+
+    void set(int i, int n) {
+        arr[i] = n;
+    }
+
+    void pushback(int n) {
+        if(size == capacity) {
+            resize();
+        }
+        arr[size++] = n;
+    }
+
+    int popback() {
+        return arr[--size];
+    }
+
+    void resize() {
+        capacity *= 2;
+        int* newArr = new int[capacity];
+        std::copy(arr, arr + size, newArr);
+        delete[] arr;
+        arr = newArr;
+    }
+
+    int getSize() {
+        return size;
+    }
+
+    int getCapacity() {
+        return capacity;
+    }
+};
